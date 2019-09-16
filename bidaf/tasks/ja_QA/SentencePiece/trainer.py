926ab024abf7b83594c59a64b2fab5e761085411
MRyo-ie <e165738@ie.u-ryukyu.ac.jp>
2019-09-17 04:34:16 +0900

    【バグ】「（SP スペース問題）デバッグ中」
    ・２

--- bidaf/tasks/ja_QA/SentencePiece/trainer.py
+++ bidaf/tasks/ja_QA/SentencePiece/trainer.py
@@ -11,7 +11,7 @@ from urllib.request import urlretrieve
 class SentencePieceTrainer():
     def __init__(self, config:configparser, wiki_extpath, model_dir_abspath):
         self.wiki_glob_path = os.path.join(wiki_extpath, '**', "wiki_*")
-        self.prefix = os.path.join( model_dir_abspath, config.get('train', 'vocab_size') )
+        self.prefix = os.path.join( model_dir_abspath, config.get('train', 'model_name') )
         self.vocab_size = config.get('train', 'vocab_size')
         if not os.path.exists(model_dir_abspath):
             os.mkdir(model_dir_abspath)
@@ -19,15 +19,27 @@ class SentencePieceTrainer():
         # Sentence Piece は、パスに スペースが入ってると動かない...。ので、一旦別の場所に移動する。
         self.is_space_in_path = ' ' in model_dir_abspath
         if self.is_space_in_path:
-            root_dir = os.path.join('tmp', 'SentencePiece')
+            print('[確認](SP_Trainer)  パスにスペースが入っていたため、回避処理を開始します。')
+            os.path.splitdrive(os.path.abspath(__file__))
+            root_dir = os.path.join(self.get_root_path(), 'tmp', 'SentencePiece')
             # Wikipedia extractデータ をコピー
             if not os.path.exists(root_dir):
                 os.mkdir(root_dir)
             shutil.copytree(wiki_extpath, root_dir)
             self.wiki_glob_path = os.path.join(root_dir, '**', "wiki_*")
             # model ファイルは、通常の場所にもコピーする。という方向で。
-            self.prefix = os.path.join( root_dir, config.get('train', 'vocab_size') )
-            self.prefix_tmp = os.path.join( model_dir_abspath, config.get('train', 'vocab_size') )
+            self.prefix = os.path.join( root_dir, config.get('train', 'model_name') )
+            self.prefix_tmp = os.path.join( model_dir_abspath, config.get('train', 'model_name') )
+
+
+    def get_root_path(self):
+        before = os.path.abspath('__file__')
+        after = os.path.split(before)[0]
+        while after != before:
+            print(after)
+            before = after
+            after = os.path.split(before)[0]
+        return after
 
 
     def _get_text_file(self):
@@ -43,4 +55,5 @@ class SentencePieceTrainer():
         sp.SentencePieceTrainer.Train(command)
         if self.is_space_in_path:
             shutil.copytree(self.prefix, self.prefix_tmp)
+            print('[確認](SP_Trainer)  {}\n        : {}'.format(self.prefix_tmp, os.listdir(self.prefix_tmp)))
 
